db10ed3599ad1df04746279289e45bccead9cde7
dknightdev <duedi707@gmail.com>
2025-04-17 13:49:14 -0500

    swagger dark mode

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,40 @@
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { join } from 'path'
 
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'
 
-import envVars from './config/env';
+import envVars from './config/env'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
-  app.useGlobalPipes(new ValidationPipe({
-    forbidNonWhitelisted: true,
-    whitelist: true,
-    transform: true,
-  }));
+	app.useGlobalPipes(new ValidationPipe({
+		forbidNonWhitelisted: true,
+		whitelist: true,
+		transform: true
+	}))
 
-  const config = new DocumentBuilder()
-    .setTitle('API Bitacora')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
+	app.useStaticAssets(join(__dirname, '..', 'public', 'swagger'), {
+		prefix: '/swagger/'
+	})
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+	const config = new DocumentBuilder()
+		.setTitle('API Bitacora')
+		.setVersion('1.0')
+		.addBearerAuth()
+		.build()
 
-  app.enableCors()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('docs', app, document, {
+		customCssUrl: '/swagger/swagger-dark.css'
+	})
 
-  await app.listen(envVars.PORT);
+	app.enableCors()
+
+	await app.listen(envVars.PORT)
 }
 
-bootstrap();
+bootstrap()
